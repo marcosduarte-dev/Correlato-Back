5dbd3d16682b82c59ef604e6a009e03753317252
Marcos Duarte <pe.marcos30@gmail.com>
2024-12-03 15:20:25 -0300

    Adicionando projeto inicial

---
+++ src/main/java/com/marcospedroso/facens/correlato/CorrelatoApplication.java
@@ -0,0 +1,13 @@
+package com.marcospedroso.facens.correlato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorrelatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorrelatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcospedroso/facens/correlato/CorrelatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcospedroso.facens.correlato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorrelatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
