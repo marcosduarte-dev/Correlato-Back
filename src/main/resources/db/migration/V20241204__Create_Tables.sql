8900ad98dc45d57c5dc8954925fa6a8931731977
Marcos Duarte <pe.marcos30@gmail.com>
2024-12-04 10:43:56 -0300

    Adicionando primeira migration

---
+++ src/main/resources/db/migration/V20241204__Create_Tables.sql
@@ -0,0 +1,48 @@
+CREATE TABLE tb_faculdade (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255),
+    ativo BOOLEAN DEFAULT true
+);
+
+CREATE TABLE tb_curso (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255),
+    id_faculdade INT,
+    ativo BOOLEAN DEFAULT true,
+    FOREIGN KEY (id_faculdade) REFERENCES tb_faculdade(id)
+);
+
+CREATE TABLE tb_disciplina (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255),
+    cod_disciplina VARCHAR(50),
+    id_curso INT,
+    carga_horaria FLOAT,
+    ementa TEXT,
+    programa TEXT,
+    ativo BOOLEAN DEFAULT true,
+    FOREIGN KEY (id_curso) REFERENCES tb_curso(id)
+);
+
+CREATE TABLE tb_usuario (
+    id UUID PRIMARY KEY,
+    email VARCHAR(255),
+    nome VARCHAR(255),
+    senha VARCHAR(255),
+    id_faculdade INT,
+    tipo VARCHAR(50),
+    ativo BOOLEAN DEFAULT true,
+    FOREIGN KEY (id_faculdade) REFERENCES tb_faculdade(id)
+);
+
+CREATE TABLE tb_analise_equivalencia (
+    id SERIAL PRIMARY KEY,
+    id_professor_responsavel UUID,
+    id_disciplina_origem INT,
+    id_disciplina_destino INT,
+    status VARCHAR(50),
+    aprovado BOOLEAN,
+    FOREIGN KEY (id_professor_responsavel) REFERENCES tb_usuario(id),
+    FOREIGN KEY (id_disciplina_origem) REFERENCES tb_disciplina(id),
+    FOREIGN KEY (id_disciplina_destino) REFERENCES tb_disciplina(id)
+);
